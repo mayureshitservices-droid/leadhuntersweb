fb15282cf3849337675beececf4a20abc2fb256d
Shreedhar Halli <mayurbusinesssolutions@gmail.com>
2026-04-13 01:59:29 +0530

    feat: register mobile and update config

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
